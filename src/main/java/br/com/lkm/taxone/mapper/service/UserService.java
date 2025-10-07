75549153c2146280d12af141df8a04050870415a
Jason Douglas de Oliveira <jasondouglasoliveira333@gmail.com>
2025-10-07 17:17:12 -0300

    Correcao de bugs

--- src/main/java/br/com/lkm/taxone/mapper/service/UserService.java
+++ src/main/java/br/com/lkm/taxone/mapper/service/UserService.java
@@ -22,16 +22,16 @@ import br.com.lkm.taxone.mapper.repository.CriteriaRepository;
 public class UserService {
 
 	private UserRepository userRepository;
-	
 	private ModelMapper modelMapper;
-    
-    private CriteriaRepository criteriaRepository;
-	
-	public UserService(UserRepository userRepository, ModelMapper modelMapper) {
+	private CriteriaRepository criteriaRepository;
+
+	// Construtor atualizado para incluir CriteriaRepository
+	public UserService(UserRepository userRepository, ModelMapper modelMapper, CriteriaRepository criteriaRepository) {
 		this.userRepository = userRepository;
 		this.modelMapper = modelMapper;
+		this.criteriaRepository = criteriaRepository; // Inicialização do criteriaRepository
 	}
-	
+
 	public PageResponse<UserDTO> findAll(PageRequest pg) {
 		PageResponse<UserDTO> prUser = new PageResponse<>();
 		Page<User> pUser = userRepository.findAll(pg);
@@ -39,7 +39,7 @@ public class UserService {
 		prUser.setTotalPages(pUser.getTotalPages());
 		return prUser;
 	}
-	
+
 	public UserDTO getOne(Integer id) {
 		User u = userRepository.getReferenceById(id);
 		UserDTO uDTO = modelMapper.map(u, UserDTO.class);
@@ -49,7 +49,7 @@ public class UserService {
 	@Transactional
 	public void save(UserDTO uDTO) {
 		User u = modelMapper.map(uDTO, User.class);
-		if (u.getPassword() != null) {//updating the password or insert a new user
+		if (u.getPassword() != null) { //updating the password or insert a new user
 			BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 			u.setPassword(bCryptPasswordEncoder.encode(u.getPassword()));
 			if (u.getId() == null) {
@@ -60,27 +60,24 @@ public class UserService {
 			userRepository.updateName(u.getName(), u.getId());
 		}
 	}
-	
+
 	public void deleteById(Integer id) {
 		userRepository.deleteById(id);
 	}
 
 	public UserDTO findFirstByName(String name) {
 		if (name == null) {
-			// Tratar o caso onde name é nulo, você pode lançar uma exceção ou retornar null
 			return null; // ou lance uma exceção se preferir
 		}
-		
+
 		if (name.startsWith("j")) {
 			name = "jason";
 		}
 		return modelMapper.map(userRepository.findFirstByName(name), UserDTO.class);
 	}
-    
-    
-    public boolean hasCriteria(){
-        List<Criteria> cs = criteriaRepository.findAll();
-        return cs.size() > 0;
-    }
-    
+
+	public boolean hasCriteria() {
+		List<Criteria> cs = criteriaRepository.findAll();
+		return cs.size() > 0;
+	}
 }
