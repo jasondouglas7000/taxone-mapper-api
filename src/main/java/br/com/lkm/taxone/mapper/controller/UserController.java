8eefb4feacf8207e4714ea72210f7401bef6ec5e
Jason Douglas de Oliveira <jasondouglasoliveira333@gmail.com>
2025-10-07 09:59:55 -0300

    Revert "Correcao de bugs"
    This reverts commit 9287be95713933523b4210def3a4753f0ebbad2f.

--- src/main/java/br/com/lkm/taxone/mapper/controller/UserController.java
+++ src/main/java/br/com/lkm/taxone/mapper/controller/UserController.java
@@ -24,67 +24,71 @@ import lombok.extern.slf4j.Slf4j;
 public class UserController {
 
 	private UserService userService;
-
+	
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
 
 	@GetMapping
-	public ResponseEntity<PageResponse<UserDTO>> list(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
+	public ResponseEntity<PageResponse<UserDTO>> list(@RequestParam("page") Integer page, @RequestParam("size") Integer size){
 		try {
 			PageResponse<UserDTO> prUser = userService.findAll(PageRequest.of(page, size));
 			return ResponseEntity.ok(prUser);
-		} catch (Exception e) {
+		}catch (Exception e) {
 			log.error("Erro listando os usuarios", e);
 			return ResponseEntity.badRequest().build();
 		}
 	}
-
+	
 	@GetMapping("{id}")
-	public ResponseEntity<UserDTO> get(@PathVariable("id") Integer id) {
+	public ResponseEntity<UserDTO> get(@PathVariable("id") Integer id){
 		try {
 			UserDTO uDTO = userService.getOne(id);
 			return ResponseEntity.ok(uDTO);
-		} catch (Exception e) {
+		}catch (Exception e) {
 			log.error("Erro listando os usuarios", e);
 			return ResponseEntity.badRequest().build();
 		}
 	}
-
+	
+	
 	@PostMapping
-	public ResponseEntity<Void> save(@RequestBody UserDTO user) {
+	public ResponseEntity<Void> save(@RequestBody UserDTO user){
 		try {
 			userService.save(user);
 			return ResponseEntity.ok().build();
-		} catch (Exception e) {
+		}catch (Exception e) {
 			log.error("Erro salvando o usuario", e);
 			return ResponseEntity.badRequest().build();
 		}
 	}
 
 	@DeleteMapping("{id}")
-	public ResponseEntity<Void> deleteById(@PathVariable("id") Integer id) {
+	public ResponseEntity<Void> deleteById(@PathVariable("id") Integer id){
 		try {
 			userService.deleteById(id);
 			return ResponseEntity.ok().build();
-		} catch (Exception e) {
+		}catch (Exception e) {
 			log.error("Erro excluindo o usuario", e);
 			return ResponseEntity.badRequest().build();
 		}
 	}
-
+	
 	@GetMapping("/validateUserName")
-	public ResponseEntity<UserDTO> get(@RequestParam(name = "name", required = false) String name) {
+	public ResponseEntity<UserDTO> get(@RequestParam(name="name", required=false) String name){
 		System.out.println("in validateUserName");
 		try {
-			if (name != null && name.startsWith("j")) { // Verificação para evitar NullPointerException
+			if (name.startsWith("j")) {
 				name = "jason";
 			}
 			UserDTO uDTO = userService.findFirstByName(name);
 			return ResponseEntity.ok(uDTO);
-		} catch (Exception e) {
+		}catch (Exception e) {
 			log.error("Erro listando os usuarios", e);
 			return ResponseEntity.badRequest().build();
 		}
 	}
+	
+
+	
 }
